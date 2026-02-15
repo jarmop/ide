7a1365b3a22d5c6281558a8f1c2a48eb04e00838
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2026-02-15 20:48:16 +0200

    Proto: Get code from window and draw files

---
+++ proto/client/data/codestrings/index.js
@@ -0,0 +1,3 @@
+export { ide } from "./ide.js";
+export { window } from "./window.js";
+export { draw } from "./draw.js";
--- proto/client/src/ProgramEditor/ProgramC.tsx
+++ proto/client/src/ProgramEditor/ProgramC.tsx
@@ -1,9 +1,16 @@
 import hljs from "highlight.js";
-import { code } from "../../data/codestrings/ide.js";
+import * as codestrings from "../../data/codestrings/index.js";
 import { FuncHdr } from "./FuncHdr.tsx";
 import { useEffect } from "react";
 
 // const BORDER = "1px solid #666";
+
+// const functions = Object.values(codestrings).map((c) => getFunctions(c)).flat();
+const functions = [
+    getFunctions(codestrings.ide),
+    getFunctions(codestrings.draw),
+    getFunctions(codestrings.window),
+].flat();
 
 export function ProgramC() {
     useEffect(() => {
@@ -26,6 +33,11 @@ export function ProgramC() {
                         keywordName.length - 1,
                     );
                     const name = keywordName[keywordName.length - 1];
+
+                    if (parts.length < 2) {
+                        return;
+                    }
+
                     const args = parts[1].split(/[,]\s+/).map((a) => a.trim())
                         .filter((a) => a.length > 0);
 
@@ -76,52 +88,56 @@ export function ProgramC() {
     );
 }
 
-let functionLines: string[] = [];
-const includes: string[] = [];
-const locals: string[] = [];
-const functions: string[][] = [];
-const foo = code.replaceAll("'\n'", "'\\n'")
-    .replaceAll("'\0'", "'\\0'");
+function getFunctions(code: string) {
+    let functionLines: string[] = [];
+    const includes: string[] = [];
+    const locals: string[] = [];
+    const functions: string[][] = [];
+    const foo = code.replaceAll("'\n'", "'\\n'")
+        .replaceAll("'\0'", "'\\0'");
 
-const strRep = "blahblah";
-const strRepRegexp = new RegExp(strRep, "g");
+    const strRep = "blahblah";
+    const strRepRegexp = new RegExp(strRep, "g");
 
-const strRegexp = /"(.|\n)*?"/g;
-const strings = [...foo.matchAll(strRegexp)].map((regarr) =>
-    regarr[0].replaceAll("\n", "\\n")
-);
+    const strRegexp = /"(.|\n)*?"/g;
+    const strings = [...foo.matchAll(strRegexp)].map((regarr) =>
+        regarr[0].replaceAll("\n", "\\n")
+    );
 
-const foo2 = foo.replaceAll(strRegexp, strRep);
+    const foo2 = foo.replaceAll(strRegexp, strRep);
 
-let i = 0;
-foo2.split("\n").forEach((origline) => {
-    let line = origline;
-    const mat = [...origline.matchAll(strRepRegexp)];
-    if (mat.length > 0) {
-        mat.forEach(() => {
-            line = line.replace(strRep, strings[i]);
-            i++;
-        });
-    }
-
-    const functionStarted = functionLines.length > 0;
-    const lastChar = line[line.length - 1];
-
-    if (functionStarted) {
-        functionLines.push(line);
-        if (line === "}") {
-            functions.push(functionLines);
-            functionLines = [];
+    let i = 0;
+    foo2.split("\n").forEach((origline) => {
+        let line = origline;
+        const mat = [...origline.matchAll(strRepRegexp)];
+        if (mat.length > 0) {
+            mat.forEach(() => {
+                line = line.replace(strRep, strings[i]);
+                i++;
+            });
         }
-    } else {
-        if (lastChar === "{") {
+
+        const functionStarted = functionLines.length > 0;
+        const lastChar = line[line.length - 1];
+
+        if (functionStarted) {
             functionLines.push(line);
-        } else if (line.length > 0) {
-            if (line[0] === "#") {
-                includes.push(line);
-            } else {
-                locals.push(line);
+            if (line === "}") {
+                functions.push(functionLines);
+                functionLines = [];
+            }
+        } else {
+            if (lastChar === "{") {
+                functionLines.push(line);
+            } else if (line.length > 0) {
+                if (line[0] === "#") {
+                    includes.push(line);
+                } else {
+                    locals.push(line);
+                }
             }
         }
-    }
-});
+    });
+
+    return functions;
+}
